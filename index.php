8aea853e939f38a140a563c96d793ab81d1a17eb
Taylor Walenczyk <tawalenczyk@ku.edu>
2017-10-08 10:11:56 -0500

    Add landing page with links and styling

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
 <html>
+<head>
+	<title>Lab 04 Index</title>
+        <link href="override.css" rel="stylesheet" type="text/css"/>
+</head>
+
 <body>
-
-<?php
-        include 'myfirstprogram.php';
-?>
-
+        <div class="header">
+            <h2>Lab 04</h2>
+            Hello! Welcome to the directory for Taylor Walenczyk's Lab 04. Check out the various exercises below!
+        </div>
+        <div class="link-row">
+            <a href="./ex_1/multiplication_table.php">Exercise 1</a>
+        </div>
+        <div class="link-row">
+            <a href="./ex_2/Quiz.html">Exercise 2</a>
+        </div>
+        <div class="link-row">
+            <a href="./ex_3/customerFrontend.html">Exercise 3</a>
+        </div>
 </body>
 </html>
-
